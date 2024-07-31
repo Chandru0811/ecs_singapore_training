f522e7c8ed58b4c13821846aedf08cb3c5adf8e6
kishoreECS <kishorekumar.ravi04@gmail.com>
2024-07-31 02:26:13 -0700

    changes about page

--- src/Pages/client/ClientAbout.js
+++ src/Pages/client/ClientAbout.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import AboutHero from '../../assets/client/about_hero_image.png';
 import AboutImage from '../../assets/client/about-imgae 1.jpg'
-import { FaSquareShareNodes } from "react-icons/fa6";
+import { LuDiamond } from "react-icons/lu";
+import { Tabs, Tab } from 'react-bootstrap';
+import selfLevel from '../../assets/client/self level.png';
+import Assignment from '../../assets/client/assignment.png';
+import Support from '../../assets/client/suppoet 24-7.png';
+import AsianStudent from '../../assets/client/About-Aisian-student-scaled.jpeg'
+import { TbMessage2Exclamation } from "react-icons/tb";
 
 
 function ClientAbout() {
@@ -14,7 +20,7 @@ function ClientAbout() {
                             <div className="row">
                                 <div className="col-md-7 col-12">
                                     <div className="about-head">
-                                        <h1 className="display-5 fw-bold">What is Courses and how valid is it?</h1>
+                                        <h1 className="display-5 fw-bold" style={{ paddingLeft: "10px" }}>What is Courses and how valid is it?</h1>
                                         <p>The meaning of production in Carlio is the creation,
                                             development, and the path to progress, and the starting point to
                                             achieve the goals that we all have the Petroforce bran
@@ -38,19 +44,23 @@ function ClientAbout() {
             </div>
             <div className="container mb-4">
                 <div className="row">
-                    <div className="col-md-6 col-12">
-                        <div className="d-flex align-items-center">
-                        <img src={AboutImage} alt='img' className="img-fluid">
-                        </img>
+                    <div className="col-md-6 col-12 mb-3 d-flex align-items-center justify-content-center">
+                        <div className="">
+                            <img src={AboutImage} alt='img' className="img-fluid">
+                            </img>
                         </div>
                     </div>
                     <div className="col-md-6 col-12">
                         <div className="d-flex text-start">
-                            <FaSquareShareNodes />
-                            <p>About Us</p>
+                            <div className="d-flex align-items-center justify-content-center">
+                                <div className="p-1 mx-2" style={{ backgroundColor: "#ec9fc2", borderRadius: "5px" }}>
+                                    <LuDiamond color='#AA205E' size={30} />
+                                </div>
+                                <p className="fw-medium">About Us</p>
+                            </div>
                         </div>
                         <div className="text-start">
-                            <h4 className="display-6 fw-bold">Our Company Overview</h4>
+                            <h3 className="fw-bold">Our Company Overview</h3>
                             <p>
                                 Carlio brand is one of the most reliable motor oil manufacturers,
                                 which is engaged in the production of high quality products with
@@ -58,19 +68,180 @@ function ClientAbout() {
                                 more information about other aspects and products of the Carlio
                                 brand, you can use the following buttons:
                             </p>
-                            <div className="d-flex justify-content-between">
-                                <button className="btn company-btn">Company</button>
-                                <button className="btn company-btn">All Courses</button>
-                                <button className="btn company-btn">Our Team</button>
+                            <div>
+                                <Tabs defaultActiveKey="company" id="fill-tab-example" className="mb-3 tab">
+                                    <Tab eventKey="company" title="Company">
+                                        <div className='row mt-5'>
+                                            <p>
+                                                The meaning of production in Carlio is the creation,
+                                                development, and the path to progress, and the starting
+                                                point to achieve the goals that we all have the Petroforce
+                                                brand, with over 20 years of experience in the oil and
+                                                petrochemical industry, we officially started our
+                                                activities in the field of design, engineering,
+                                                construction of refinery equipment, and the production
+                                                of various motor and industrial lubricants in the year
+                                                1390 (2011)
+                                            </p>
+                                        </div>
+                                    </Tab>
+                                    <Tab eventKey="allCourses" title="All Courses">
+                                        <div className='row mt-5'>
+                                            <p>
+                                                The meaning of production in Carlio is the creation,
+                                                development, and the path to progress, and the starting
+                                                point to achieve the goals that we all have the Petroforce
+                                                brand, with over 20 years of experience in the oil and
+                                                petrochemical industry, we officially started our
+                                                activities in the field of design, engineering,
+                                                construction of refinery equipment, and the production
+                                                of various motor and industrial lubricants in the year
+                                                1390 (2011)
+                                            </p>
+                                        </div>
+                                    </Tab>
+                                    <Tab eventKey="ourTeam" title="Our Team">
+                                        <div className='row mt-5'>
+                                            <p>
+                                                The meaning of production in Carlio is the creation,
+                                                development, and the path to progress, and the starting
+                                                point to achieve the goals that we all have the Petroforce
+                                                brand, with over 20 years of experience in the oil and
+                                                petrochemical industry, we officially started our
+                                                activities in the field of design, engineering,
+                                                construction of refinery equipment, and the production
+                                                of various motor and industrial lubricants in the year
+                                                1390 (2011)
+                                            </p>
+                                        </div>
+                                    </Tab>
+                                </Tabs>
+                                <button className="learn-btn">Learn More</button>
                             </div>
-                            <hr></hr>
-                            <p>
-                                The meaning of production in Carlio is the creation, development,
-                                and the path to progress, and the starting point to achieve the
-                                goals that we all have the Petroforce brand, with over 20 years
-                                of experience.
-                            </p>
-                            <button className="btn learn-btn">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid mb-4">
+                <div className="row about-banner1">
+                    <div className="content-wrapper1">
+                        <div className="container">
+                            <div className="row mb-4">
+                                <div className="col-md-6 col-12 mb-3 d-flex flex-column align-items-start justify-content-center">
+                                    <div className="about-head1">
+                                        <div>
+                                            <p>Courses Features</p>
+                                        </div>
+                                        <h1 className="display-5 fw-bold">The feature of Courses that you will benefit from</h1>
+                                    </div>
+                                    <button className="btn contact-btn">All Courses</button>
+                                </div>
+                                <div className="col-md-6 col-12 d-flex align-items-center justify-content-center">
+                                    <div className="row">
+                                        <div className="col-md-4 col-4">
+                                            <div className="card about-card h-100">
+                                                <img src={selfLevel} alt='About Hero' className="img-fluid"></img>
+                                                <p className="about-textWhite">Self-level Beginner</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-4 col-4">
+                                            <div className="card about-card h-100">
+                                                <img src={Assignment} alt='About Hero' className="img-fluid"></img>
+                                                <p className="about-textWhite">Assignment Provide</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-4 col-4">
+                                            <div className="card about-card h-100">
+                                                <img src={Support} alt='About Hero' className="img-fluid"></img>
+                                                <p className="about-textWhite">Support 24/7</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container mb-4">
+                <div className="row">
+                    <div className="col-md-6 col-12 mb-3 d-flex align-items-center justify-content-center">
+                        <div className="" >
+                            <img src={AsianStudent} alt='img' className="img-fluid">
+                            </img>
+                        </div>
+                    </div>
+                    <div className="col-md-6 col-12">
+                        <div className="d-flex text-start">
+                            <div className="d-flex align-items-center justify-content-center">
+                                <div className="p-1 mx-2 mb-3" style={{ backgroundColor: "#ec9fc2", borderRadius: "5px" }}>
+                                    <TbMessage2Exclamation color='#AA205E' size={30} />
+                                </div>
+                                <p className="fw-medium">FAQ Question</p>
+                            </div>
+
+                        </div>
+                        <div className="text-start">
+                            <h3 className="fw-bold mb-3">Frequently Asked Questions</h3>
+                        </div>
+                        <div className="accordion" id="accordionExample">
+                            <div className="accordion-item mb-3" style={{paddingLeft: "10px"}}>
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Lorem Ipsum is simply dummy text of the prin....?
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body text-start">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item mb-3" style={{paddingLeft: "10px"}}>
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    Lorem Ipsum is simply dummy text of the prin....?
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body text-start">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item mb-3" style={{paddingLeft: "10px"}}>
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Lorem Ipsum is simply dummy text of the prin....?
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body text-start">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item mb-3" style={{paddingLeft: "10px"}}>
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Lorem Ipsum is simply dummy text of the prin....?
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body text-start">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                                    </div>
+                                </div>
+                            </div>
+                            <div className="accordion-item mb-3" style={{paddingLeft: "10px"}}>
+                                <h2 className="accordion-header">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    Lorem Ipsum is simply dummy text of the prin....?
+                                    </button>
+                                </h2>
+                                <div id="collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div className="accordion-body text-start">
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
