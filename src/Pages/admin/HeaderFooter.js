1122653f3ed5abd231727ad1c03b1e726c24c500
manojecs2020 <manoj@ecscloudinfotech.com>
2024-08-02 10:59:37 +0530

    bug

--- src/Pages/admin/HeaderFooter.js
+++ src/Pages/admin/HeaderFooter.js
@@ -24,7 +24,7 @@ import { LiaTelegram } from "react-icons/lia";
 import { FiFigma } from "react-icons/fi";
 import { FiGitlab } from "react-icons/fi";
 import { FiGithub } from "react-icons/fi";
-import { FaEdit, FaSave, FaTimes } from 'react-icons/fa';
+import { FaEdit, FaSave, FaTimes } from "react-icons/fa";
 
 const HeaderFooter = ({ handleLogin }) => {
   const [course, setCourse] = useState(false);
@@ -85,7 +85,12 @@ const HeaderFooter = ({ handleLogin }) => {
   ];
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (courseRef.current &&courseRef2.current && !courseRef.current.contains(event.target) && !courseRef2.current.contains(event.target)) {
+      if (
+        courseRef.current &&
+        courseRef2.current &&
+        !courseRef.current.contains(event.target) &&
+        !courseRef2.current.contains(event.target)
+      ) {
         setCourse(false);
       }
     };
@@ -96,16 +101,22 @@ const HeaderFooter = ({ handleLogin }) => {
     };
   }, []);
 
- 
   return (
-    <>
-     <div className="" style={{ position: "sticky", top: "0", zIndex: "999" }}>
+    <div className="container-fluid px-0 pt-2">
+      <div >
+      <div className="card-header d-flex align-items-center px-0 py-2 mb-2 bg-light">
+        <h3 className="fw-bold">Header</h3>
+        <div className="container-fluid d-flex justify-content-end">
+          <button className="btn btn-sm btn-danger mx-2">Publish</button>
+        </div>
+      </div>
+      <div className="" style={{ position: "sticky", top: "0", zIndex: "999" }}>
         <p className="mb-0 text-light fw-light topHeader">
           We offer Job Gurantee Courses (Any Degree/Diploma Canditates/Year
           Gap/Non IT/Any Passed outs)
         </p>
         <Navbar bg="light" expand="lg" className="clientNav shadow-lg">
-          <Navbar.Brand  className="ms-3">
+          <Navbar.Brand className="ms-3">
             <img
               src={logo}
               height="40"
@@ -118,10 +129,7 @@ const HeaderFooter = ({ handleLogin }) => {
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav " />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className=" "
-          >
+          <Navbar.Collapse id="basic-navbar-nav" className=" ">
             <Nav className="gap-3">
               <div ref={courseRef2}>
                 <button
@@ -132,7 +140,10 @@ const HeaderFooter = ({ handleLogin }) => {
                   {course ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
                 </button>
               </div>
-              <div className="form-group position-relative headerInput" style={{width:"40vw"}}>
+              <div
+                className="form-group position-relative headerInput"
+                style={{ width: "40vw" }}
+              >
                 <input
                   type="text"
                   className="form-control"
@@ -142,8 +153,11 @@ const HeaderFooter = ({ handleLogin }) => {
                   <FaSearch className="" />
                 </span>
               </div>
-              </Nav>
-              <Nav className=" gap-4 justify-content-end me-2" style={{flexGrow: "inherit !important"}}>
+            </Nav>
+            <Nav
+              className=" gap-4 justify-content-end me-2"
+              style={{ flexGrow: "inherit !important" }}
+            >
               <Nav.Link as={NavLink} to="/home">
                 Home
               </Nav.Link>
@@ -156,9 +170,7 @@ const HeaderFooter = ({ handleLogin }) => {
               </Nav.Link>
 
               <Link to="/login">
-                <Button  className="loginBtn">
-                  Login
-                </Button>
+                <Button className="loginBtn">Login</Button>
               </Link>
             </Nav>
           </Navbar.Collapse>
@@ -196,106 +208,134 @@ const HeaderFooter = ({ handleLogin }) => {
           </div>
         </div>
       )}
-
-<div
-      className="container-fluid text-light "
-      style={{ backgroundColor: "#31135E" }}
-    >
-      <div className=" row px-3 pt-5">
-        <div className="col-md-3 col-12  mb-0 ">
-          <div className="">
-          <img src={logo} alt="ECS Training" className="img-fluid h-25 w-25 mb-3" />
-          <span>ECS</span><span>Training</span>
+      </div>
+      <div className="" style={{marginTop:"5rem"}}>
+        <div className="card-header d-flex align-items-center py-2  mb-2 bg-light">
+          <h3 className="fw-bold">Footer</h3>
+          <div className="container-fluid d-flex justify-content-end">
+            <button className="btn btn-sm btn-danger mx-2">Publish</button>
           </div>
-          <p className="fw-light text-start">
-            Hi! My name is Dmitrii Rogozza and I’m an expert in web design and
-            branding. I can help you make your website more attractive.
-          </p>
         </div>
-        <div className="col-md-3 col-12  mb-0 text-start">
-          <h5 className="mb-4 ms-1 text-start">Contact Us</h5>
-          <div className="row ">
-            <div className="col-auto pe-0">
-              <IoLocationOutline size={20} />
-            </div>
-            <div className="col ps-0">
-              <p className="fw-light text-start mb-3 ms-1">
-                Wisconsin Ave, Suite 700 Chevy Chase, Maryland 20815
+
+        <div
+          className="container-fluid text-light "
+          style={{ backgroundColor: "#31135E" }}
+        >
+          <div className=" row px-3 pt-5">
+            <div className="col-md-3 col-12  mb-0 ">
+              <div className="">
+                <img
+                  src={logo}
+                  alt="ECS Training"
+                  className="img-fluid h-25 w-25 mb-3"
+                />
+                <span>ECS</span>
+                <span>Training</span>
+              </div>
+              <p className="fw-light text-start">
+                Hi! My name is Dmitrii Rogozza and I’m an expert in web design
+                and branding. I can help you make your website more attractive.
               </p>
             </div>
-          </div>
-          <div className="row ">
-            <div className="col-auto pe-0">
-              <TbMail size={20} />
+            <div className="col-md-3 col-12  mb-0 text-start">
+              <h5 className="mb-4 ms-1 text-start">Contact Us</h5>
+              <div className="row ">
+                <div className="col-auto pe-0">
+                  <IoLocationOutline size={20} />
+                </div>
+                <div className="col ps-0">
+                  <p className="fw-light text-start mb-3 ms-1">
+                    Wisconsin Ave, Suite 700 Chevy Chase, Maryland 20815
+                  </p>
+                </div>
+              </div>
+              <div className="row ">
+                <div className="col-auto pe-0">
+                  <TbMail size={20} />
+                </div>
+                <div className="col ps-0">
+                  <p className="fw-light text-start ms-1">support@figma.com</p>
+                </div>
+              </div>
             </div>
-            <div className="col ps-0">
-              <p className="fw-light text-start ms-1">support@figma.com</p>
+            <div className="col-md-2 col-6 mb-md-0 text-start">
+              <h5 className="mb-4 ">For Businesses</h5>
+              <ul className="list-unstyled ">
+                <Nav.Link href="#job-post" className="mb-2">
+                  Job Post
+                </Nav.Link>
+                <Nav.Link href="#courses" className="mb-2">
+                  Courses
+                </Nav.Link>
+                <Nav.Link href="##careers" className="mb-2">
+                  Careers
+                </Nav.Link>
+              </ul>
+            </div>
+            <div className="col-md-2 col-6 mb-md-0 text-start">
+              <h5 className="mb-4 ">Company</h5>
+              <ul className="list-unstyled">
+                <div className="d-flex flex-column">
+                  <Nav.Link href="#about" className="mb-2">
+                    About
+                  </Nav.Link>
+                  <Nav.Link href="#blogs" className="mb-2">
+                    Blogs
+                  </Nav.Link>
+                  <Nav.Link href="#contact-us" className="mb-2">
+                    Contact Us
+                  </Nav.Link>
+                </div>
+              </ul>
+            </div>
+            <div className="col-md-2 col-12 text-start mb-3">
+              <h5 className="mb-4 ">Let's do it!</h5>
+              <ul className="footer-social list-inline ps-0">
+                <div className="d-flex flex-wrap gap-2 ">
+                  <Nav.Link href="#facebook">
+                    <LuFacebook />
+                  </Nav.Link>
+                  <Nav.Link href="#Gitlab">
+                    <FiGitlab />
+                  </Nav.Link>
+                  <Nav.Link href="#Github">
+                    <FiGithub />
+                  </Nav.Link>
+                  <Nav.Link href="#Instagram">
+                    <GrInstagram />
+                  </Nav.Link>
+                  <Nav.Link href="#Telegram">
+                    <LiaTelegram />
+                  </Nav.Link>
+                  <Nav.Link href="#Figma">
+                    <FiFigma />
+                  </Nav.Link>
+                </div>
+              </ul>
+              <button className="footer-btn border-0 rounded-pill mt-3 px-3 py-2">
+                Enroll Now
+              </button>
+            </div>
+          </div>
+          <hr className="mt-0 mb-3" />
+          <div className="container-fluid">
+            <div className=" row pb-3">
+              <div className="col-md-6 col-12">
+                <div className="">
+                  <span className="me-3">Privacy Policy</span>
+                  <span>Terms of Use</span>
+                </div>
+              </div>
+              <div className="col-md-6 col-12 text-md-end text-start">
+                <p className="mb-0" style={{ color: "#a0a0a0" }}>
+                  &copy; 2021 All Rights Reserved
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-md-2 col-6 mb-md-0 text-start">
-          <h5 className="mb-4 ">For Businesses</h5>
-          <ul className="list-unstyled ">
-          <Nav.Link href="#job-post" className="mb-2">Job Post</Nav.Link>
-          <Nav.Link href="#courses" className="mb-2">Courses</Nav.Link>
-          <Nav.Link href="##careers" className="mb-2">Careers</Nav.Link>
-          </ul>
-        </div>
-        <div className="col-md-2 col-6 mb-md-0 text-start">
-          <h5 className="mb-4 ">Company</h5>
-          <ul className="list-unstyled">
-            <div className="d-flex flex-column">
-          <Nav.Link href="#about" className="mb-2">About</Nav.Link>
-          <Nav.Link href="#blogs" className="mb-2">Blogs</Nav.Link>
-          <Nav.Link href="#contact-us" className="mb-2">Contact Us</Nav.Link>
-          </div>
-          </ul>
-        </div>
-        <div className="col-md-2 col-12 text-start mb-3">
-          <h5 className="mb-4 ">Let's do it!</h5>
-          <ul className="footer-social list-inline ps-0">
-          <div className="d-flex flex-wrap gap-2 ">
-          <Nav.Link href="#facebook">
-            <LuFacebook />
-          </Nav.Link>
-          <Nav.Link href="#Gitlab">
-            <FiGitlab />
-          </Nav.Link>
-          <Nav.Link href="#Github">
-            <FiGithub />
-          </Nav.Link>
-          <Nav.Link href="#Instagram">
-            <GrInstagram />
-          </Nav.Link>
-          <Nav.Link href="#Telegram">
-            <LiaTelegram />
-          </Nav.Link>
-          <Nav.Link href="#Figma">
-            <FiFigma />
-          </Nav.Link>
-          </div>
-          </ul>
-          <button className="footer-btn border-0 rounded-pill mt-3 px-3 py-2">Enroll Now</button>
-        </div>
-      </div>
-      <hr className="mt-0 mb-3" />
-      <div className="container-fluid">
-      <div className=" row pb-3">
-        <div className="col-md-6 col-12">
-          <div className="">
-            <span className="me-3">Privacy Policy</span>
-            <span>Terms of Use</span>
-          </div>
-        </div>
-        <div className="col-md-6 col-12 text-md-end text-start">
-          <p className="mb-0" style={{ color: "#a0a0a0" }}>
-            &copy; 2021 All Rights Reserved
-          </p>
-        </div>
-      </div>
       </div>
     </div>
-    </>
   );
 };
 
